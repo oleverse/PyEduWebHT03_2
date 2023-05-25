b59e86261e96cf74d052bd78e00636833353abd8
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-05-25 15:04:42 +0300

    Synchronous version

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+from time import time
+
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+
+def factorize(*number):
+    result = {}
+
+    for n in number:
+        potential_divider = n
+        result[n] = list()
+        while potential_divider >= 1:
+            if not (n % potential_divider):
+                result[n].insert(0, potential_divider)
+            potential_divider -= 1
+
+    return tuple(list(result.values()))
+
+
+start = time()
+a, b, c, d = factorize(128, 255, 99999, 10651060)
+logger.debug(f"Synchronous time: {time() - start} s")
+
+assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
