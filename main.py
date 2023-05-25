ab145489e8170f5cfe9cf158b7cc8e924bd87d30
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-05-25 16:10:56 +0300

    pool.map version: yet the worst one

--- main.py
+++ main.py
@@ -20,40 +20,49 @@ def factorize_distinct(number):
     return result
 
 
-def factorize(*number, synch=False):
+def factorize(*number):
     result = list()
 
-    if synch:
-        # cpu_count() call is unnecessary, Pool uses all cores by default
-        pool = Pool(multiprocessing.cpu_count())
-        async_results = [pool.apply_async(factorize_distinct, (n,)) for n in number]
+    for n in number:
+        result.append(factorize_distinct(n))
 
-        for async_res in async_results:
-            result.append(async_res.get())
-
-        return tuple(result)
-
-    else:
-        for n in number:
-            result.append(factorize_distinct(n))
-
-        return tuple(result)
+    return tuple(result)
 
 
-start = time()
-a, b, c, d = factorize(128, 255, 99999, 10651060, synch=True)
-logger.debug(f"Aynchronous time: {time() - start} s")
+def is_divided(arg_pair):
+    number, divider = arg_pair
+    if not number % divider:
+        return divider
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-start = time()
-a, b, c, d = factorize(128, 255, 99999, 10651060)
-logger.debug(f"Synchronous time: {time() - start} s")
+def factorize_async(*number):
+    result = list()
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    pool = Pool(50)
+    for n in number:
+        res = pool.map(is_divided, [(n, divider) for divider in range(1, n+1)])
+        result.append(list(filter(lambda r: r is not None, res)))
+
+    return tuple(result)
+
+
+if __name__ == "__main__":
+    start = time()
+    a, b, c, d = factorize_async(128, 255, 99999, 10651060)
+    logger.debug(f"Aynchronous time: {time() - start} s")
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
+                 1521580, 2130212, 2662765, 5325530, 10651060]
+
+    start = time()
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
+    logger.debug(f"Synchronous time: {time() - start} s")
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
+                 1521580, 2130212, 2662765, 5325530, 10651060]
